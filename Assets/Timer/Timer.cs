7a6ed1480afc7a8c302fc2b9703f14f561eadd9a
村上和希 <vestmankazuki@gmail.com>
2020-02-01 06:26:45 +0900

    update : Timer

--- Assets/Timer/Timer.cs
+++ Assets/Timer/Timer.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    public float sec;
+    [SerializeField,Header("残り時間表示用Text")]
+    private Text text;
+    [SerializeField,Header("残り時間ゲージ")]
+    private Image gage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(CT_Timer());
     }
 
-    // Update is called once per frame
-    void Update()
+
+    /// <summary>
+    /// タイマー処理
+    /// </summary>
+    IEnumerator CT_Timer()
     {
-        
+        for (float f = 0.0f; f < sec; f += 1.0f * Time.deltaTime)
+        {
+            gage.fillAmount = 1f - (f / sec);
+            text.text = "" + (int)(sec - f);
+            yield return null;
+        }
     }
 }
