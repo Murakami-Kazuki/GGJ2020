ca3d50f93581bdb3c0bb1925bc39b7e6c271b9fe
kenya <kobayashikenya@na.local>
2020-02-01 10:23:07 +0900

    added audio

---
+++ Assets/kobayashi/Script/TitleAudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Title
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class TitleAudio : MonoBehaviour
+    {
+        [SerializeField] AudioClip bgm;
+        [SerializeField] AudioClip buttonPress;
+
+        AudioSource myAudio;
+
+        void Awake()
+        {
+            myAudio = GetComponent<AudioSource>();
+            TitleControl.Instance.onNextSceneAction += NextSceneActionCallback;
+        }
+
+        void StartBGM()
+        {
+            myAudio.clip = bgm;
+            myAudio.Play();
+        }
+
+        void NextSceneActionCallback()
+        {
+            StopBGM();
+        }
+
+        void StopBGM()
+        {
+            myAudio.Stop();
+            Debug.Log("BGM stop");
+        }
+    }
+}
--- Assets/kobayashi/Script/TitleControl.cs
+++ Assets/kobayashi/Script/TitleControl.cs
@@ -1,22 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace Title
 {
     public class TitleControl : MonoBehaviour
     {
 
+        protected static TitleControl instance;
+        public static TitleControl Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = (TitleControl)FindObjectOfType(typeof(TitleControl));
+                    if (instance == null)
+                    {
+                        var name = typeof(TitleControl).Name;
+                        Debug.LogFormat("Create singleton object: {0}", name);
+                        GameObject obj = new GameObject(name);
+                        instance = obj.AddComponent<TitleControl>();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public Action onNextSceneAction;
         void Update()
         {
+            if (IsPressButton()) OnTapAnyButton();
+        }
 
+        bool IsPressButton()
+        {
             //TODO 入力受付
+            if (Input.GetMouseButton(0)) return true;
+            return false;
         }
 
         void OnTapAnyButton()
         {
             //TODO scene移動
+            TransNextScene();
+        }
+
+
+        void TransNextScene()
+        {
             Debug.Log("ok. trans next scene.");
+
+            onNextSceneAction();
         }
     }
 }
