74e85a5d6a9f1c8e7fb2d352f7306f5e479b0be5
kenya <kobayashikenya@na.local>
2020-02-01 14:53:38 +0900

    update fademaanger

--- Assets/kobayashi/Result/Scripts/ResultCanvas.cs
+++ Assets/kobayashi/Result/Scripts/ResultCanvas.cs
@@ -3,38 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ResultCanvas : MonoBehaviour
+namespace Result
 {
-    [SerializeField] HairResult hairResult;
-    [SerializeField] List<Text> pointList;
-    [SerializeField] Text resultText;
-
-    [SerializeField] Image background;
-
-    void Start()
+    public class ResultCanvas : MonoBehaviour
     {
-        background.gameObject.SetActive(false);
-        //ShowResult(AnnounceType.MojaMoja);
-    }
+        [SerializeField] HairResult hairResult;
+        [SerializeField] List<Text> pointList;
+        [SerializeField] Text resultText;
 
-    /// <summary>
-    // リザルト画面を表示
-    /// </summary>
-    public void ShowResult(AnnounceType type)
-    {
-        var dict = hairResult.MyHairDict;
-        pointList[0].text = dict[HairType.SaraSara].ToString();
-        pointList[1].text = dict[HairType.GachiGachi].ToString();
-        pointList[2].text = dict[HairType.MojaMoja].ToString();
+        [SerializeField] Image background;
 
-        resultText.text = hairResult.GetResult(type);
-        StartCoroutine(Show());
-    }
+        void Start()
+        {
+            background.gameObject.SetActive(false);
+            //ShowResult(AnnounceType.MojaMoja);
+            FadeManager.Instance.Fade(Color.white, new Color(1, 1, 1, 0), 1f);
+        }
 
-    IEnumerator Show()
-    {
-        //TODO アニメーションを追加
-        background.gameObject.SetActive(true);
-        yield return null;
+        /// <summary>
+        // リザルト画面を表示
+        /// </summary>
+        public void ShowResult(AnnounceType type)
+        {
+            var dict = hairResult.MyHairDict;
+            pointList[0].text = dict[HairType.SaraSara].ToString();
+            pointList[1].text = dict[HairType.GachiGachi].ToString();
+            pointList[2].text = dict[HairType.MojaMoja].ToString();
+
+            resultText.text = hairResult.GetResult(type);
+            StartCoroutine(Show());
+        }
+
+        IEnumerator Show()
+        {
+            //TODO アニメーションを追加
+            background.gameObject.SetActive(true);
+            yield return null;
+        }
     }
 }
