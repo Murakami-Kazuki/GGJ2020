cb2573c77b12ecc5292cebed7812c35ad7577aa1
kenya <kobayashikenya@na.local>
2020-02-01 09:54:48 +0900

    added title control

---
+++ Assets/kobayashi/Script/TitleControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Title
+{
+    public class TitleControl : MonoBehaviour
+    {
+
+        void Update()
+        {
+
+            //TODO 入力受付
+        }
+
+        void OnTapAnyButton()
+        {
+            //TODO scene移動
+            Debug.Log("ok. trans next scene.");
+        }
+    }
+}
