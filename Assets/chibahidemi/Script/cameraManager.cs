57282fd4ad0ae5ea8dea8093a38702e7efb956a6
千葉日出実 <ccp.yyrah1209@gmail.com>
2020-02-01 17:53:22 +0900

    色々追加

---
+++ Assets/chibahidemi/Script/cameraManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraManager : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 cameraPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+        transform.position = new Vector3(playerPos.x, cameraPos.y, playerPos.z+cameraPos.z);
+    }
+}
