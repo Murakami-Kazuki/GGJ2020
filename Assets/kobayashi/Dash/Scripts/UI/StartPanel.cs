6e862ecb279e253afde881738033f110b7530f14
kenya <kobayashikenya@na.local>
2020-02-01 17:12:13 +0900

    added countdown

---
+++ Assets/kobayashi/Dash/Scripts/UI/StartPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dash
+{
+    public class StartPanel : BasePanel
+    {
+        [SerializeField] Text countdownText;
+
+        public override void Activate()
+        {
+            base.Activate();
+
+            StartCoroutine(Countdown(3));
+        }
+
+        IEnumerator Countdown(int count)
+        {
+            while (0 < count)
+            {
+                countdownText.text = count.ToString();
+                yield return new WaitForSeconds(1f);
+                count--;
+            }
+
+            countdownText.text = "0";
+
+        }
+    }
+}
